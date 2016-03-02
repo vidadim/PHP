a581d13f08c889565c74909eff6e1b9e5dde5b65
Orkhan Abbasli <C:\Users\a.orkhan\AppData\Roaming\The Bat! Pwd>
2016-03-02 10:32:32 +0400

    fuck this shit

---
+++ Lesson01/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LESSON31 - PHP LESSON01</title>
+</head>
+<body>
+	<?php 
+		echo "Hello World";
+ 	?>
+</body>
+</html>
---
+++ Lesson02/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LESSON2 PHP</title>
+</head>
+<body>
+
+	<?php
+		echo "LESSON2";
+	?>
+
+</body>
+</html>
